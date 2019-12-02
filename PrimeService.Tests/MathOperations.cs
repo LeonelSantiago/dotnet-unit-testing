b70e99549f82b4bfc678b5c23980bf1f40eda5f0
Leonel Antonio Santiago Baez <santiagobaez@msn.com>
2019-12-02 15:47:18 -0400

    Add of basic test cases with mathematical operationes in XUnit Tesst Project

---
+++ PrimeService.Tests/MathOperations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PrimeService.Tests
+{
+    public class MathOperations
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+
+    }
+}
