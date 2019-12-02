698d753470cca6bc12689bc27cf2d0c75823ef28
Leonel Antonio Santiago Baez <santiagobaez@msn.com>
2019-12-02 10:43:07 -0400

    Add of method with example validation

--- PrimeService/PrimeService.cs
+++ PrimeService/PrimeService.cs
@@ -4,5 +4,13 @@ namespace PrimeService
 {
     public class PrimeService
     {
+        public bool IsPrime(int candidate)
+        {
+            if(candidate < 2)
+            {
+                return false;
+            }
+            throw new NotImplementedException("Please create a test first.");
+        }
     }
 }
